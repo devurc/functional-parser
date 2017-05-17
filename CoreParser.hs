3827435c688653cffe6de407726fbc232f9d2c37
Cleo Schurink <Cleo Schurink>
2017-05-17 12:30:31 +0200

    Skeleton files

---
+++ CoreParser.hs
@@ -0,0 +1,128 @@
+------------------------------------------------------------------------
+--Defines the Parser type and implements the 3 elementary parsers, char, return and fail.
+--Also basic parser operators.
+--Class Parse with signatures for parse, toString and fromString
+--Implementation for the last one is introduced??
+--The representation of the Parser type is visible outside the module, but
+--this visibility should not be exploited
+
+-- FIRST PARSER MODULE --
+-- This module should contain the:
+-- - parser type
+-- - basic parsers
+-- - parser operators
+
+-- the representation of parsers as values of type String -> Maybe(a, String)
+-- will not be exploited outside this module
+
+-- is useful to have a type class declaring the names parse and toString and
+-- defining fromString
+-- We then can use these names for all the datatypes. **Parse** is the name of this
+-- type class.
+------------------------------------------------------------------------
+
+module CoreParser(Parser, char, return, fail, (#), (!), (?), (#>), (>->),
+                  Parse, parse, toString, fromString) where
+import Prelude hiding (return, fail)
+infixl 3 !
+infixl 7 ?
+infixl 6 #
+infixl 5 >->
+infixl 4 #>
+
+class Parse a where
+    parse :: Parser a
+    fromString :: String -> a
+    fromString cs =
+        case parse cs of
+               Just(s, []) -> s
+               Just(s, cs) -> error ("garbage '"++cs++"'")
+               Nothing -> error "Nothing"
+    toString :: a -> String
+
+type Parser a = String -> Maybe (a, String)
+
+
+----CHAR PARSER----
+--Basic parser that will accept only one character
+--Synonym for String -> Maybe(Char, String)
+--Function will fail if the input string is empty
+
+char :: Parser Char
+char []= Nothing
+char (c:cs) = Just (c, cs)
+
+----RETURN PARSER----
+--Return will always succeed without inspecting the input string.
+--It returns the first argument which may be of any type.
+
+return :: a -> Parser a
+return a cs = Just (a, cs)
+
+----FAIL PARSER----
+--This parser will never accept any input. Fail is hidden when importing prelude.
+
+fail ::  Parser a
+fail cs = Nothing
+
+------PARSER OPERATORS-------
+
+----ALTERNATIVES IN GRAMMAR----
+--! is an infix operator
+--example: (m ! n)
+--If m is applied to the input string and succeeds then thats it
+--If it fails then apply n to input string
+--AKA try m first then n
+
+(!) :: Parser a -> Parser a -> Parser a
+(m ! n) cs = case m cs of
+             Nothing -> n cs
+             mcs -> mcs
+
+--? is also an infix operator
+--example: (m ? p) is a parser which applies m to an input string and tests
+--if the result satisfies p
+
+(?) :: Parser a -> (a -> Bool) -> Parser a
+(m ? p) cs =
+    case m cs of
+    Nothing -> Nothing
+    Just(r, s) -> if p r then Just(r, s) else Nothing
+
+--Applies two parsers in sequence where the remainder string from the first
+--one is fed into the other
+--The results of the two parsers are combined into a pair.
+
+(#) :: Parser a -> Parser b -> Parser (a, b)
+(m # n) cs =
+    case m cs of
+    Nothing -> Nothing
+    Just(a, cs') ->
+        case n cs' of
+        Nothing -> Nothing
+        Just(b, cs'') -> Just((a, b), cs'')
+
+--Transforming the result of the parser. The right operand of >-> named k is
+--the function defining the transformation. The result of the operation is a
+--new parser.
+--We may use it to define a parser which accepts a digit and returns an Int
+--using digitToInt from the prelude
+
+(>->) :: Parser a -> (a -> b) -> Parser b
+(m >-> b) cs =
+    case m cs of
+    Just(a, cs') -> Just(b a, cs')
+    Nothing -> Nothing
+
+--(#-) only will apply two parsers in sequence as # but throws away the result
+--from the second one
+
+--But sometimes we need to make the result from one parser available to another.
+--After applying the parser p to the input string, both the result and the
+--remainder input string are given to the second operand - k
+
+(#>) :: Parser a -> (a -> Parser b) -> Parser b
+(p #> k) cs =
+    case p cs of
+    Nothing -> Nothing
+    Just(a, cs') -> k a cs'
---
+++ Parser.hs
@@ -0,0 +1,126 @@
+------------------------------------------------------------------------
+--Contains a number of derived parsers and parser operators
+
+------ ? is a condition checker
+------ ! if the first one fails, do the second
+------ # take the result of both parsers and smash into a pair
+------ >-> LHS would be parser result and RHS would be a transformation
+------ defining function.
+
+------ The derived parsers appear here (shit is imported from CoreParser)
+------------------------------------------------------------------------
+
+module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
+              lit, number, iter, accept, require, token,
+              spaces, word, (-#), (#-)) where
+import Prelude hiding (return, fail, iterate) --Hiding the iterate function!!!
+import Data.Char
+import CoreParser
+infixl 7 -#, #-
+
+type T a = Parser a
+
+err :: String -> Parser a
+err message cs = error (message++" near "++cs++"\n")
+
+iter :: Parser a -> Parser [a]
+iter m = m # iter m >-> cons ! return []
+
+iterate :: Parser a -> Int -> Parser [a]
+iterate m 0 = return []
+iterate m i = m # iterate m (i-1) >-> cons
+
+cons(a, b) = a:b
+
+------------------------------------------------------------------------
+--applies two parsers in sequence as # but throws away the result from the first one
+--use # and >-> in the definition
+--example: (char -# char) "abc" -> Just(’b’, "c")
+--         (char -# char) "a" -> Nothing
+
+--# means that the results of the two parsers are combined into a pair.
+
+-- >-> is used to transform the result of a parser. The right operand of >-> named
+-- k is the FUNCTION defining the transformation. The result of the operation is a
+-- new parser.
+
+-So make the two parsers into a pair and extract the first or the second.
+------------------------------------------------------------------------
+
+(-#) :: Parser a -> Parser b -> Parser b
+m -# n = (m # n) >-> snd
+
+(#-) :: Parser a -> Parser b -> Parser a
+m #- n = (m # n) >-> fst
+
+------------------------------------------------------------------------
+--Gotta accept any number of whitespace characters as defined by isSpace
+--Might need to use iter?
+--iterate f x returns an infinite list of repeated applications of f to x
+------------------------------------------------------------------------
+
+spaces :: Parser String
+spaces =  iter isSpace String
+--Not sure if correct like is String even the right keyword to use??
+
+token :: Parser a -> Parser a
+token m = m #- spaces
+
+------------------------------------------------------------------------
+--a parser for a letter as defined by the prelude function isAlpha
+--Returns true if the character is an alphabetic letter
+-- Char -> Bool
+--the infix operator ? is a condition checker sorta
+------------------------------------------------------------------------
+
+letter :: Parser Char
+letter =  char ? isAlpha
+
+word :: Parser String
+word = token (letter # iter letter >-> cons)
+
+------------------------------------------------------------------------
+--The parser chars n accepts n characters
+--idk if this will work either
+------------------------------------------------------------------------
+chars :: Int -> Parser String
+chars n =  iterate Char n letter
+
+accept :: String -> Parser String
+accept w = (token (chars (length w))) ? (==w)
+
+------------------------------------------------------------------------
+--my interpretation of accept w:
+--Token is defined as ""token m = m #- spaces""
+--Chars is defined as iterate Char n letter ==my definition so might not work==
+--Takes in a string w
+--Chars accepts the whole length of the string
+--The token keyword strips the string of white spaces
+--Then checks that this string satisfies the condition of being the same as the original w
+
+--The parser require w accepts the same string as accept w but reports
+--the missing string using err in case of failure. uh yeah so wtf
+--this parser behaves as accept but emits an error message instead of
+--returning nothing
+
+--err is defined as err message cs = error (message++" near "++cs++"\n")
+--if accept w fails then continue to error message via (!)
+------------------------------------------------------------------------
+
+require :: String -> Parser String
+require w  = accept w ! err "There is a missing string??"
+
+lit :: Char -> Parser Char
+lit c = token char ? (==c)
+
+digit :: Parser Char
+digit = char ? isDigit
+
+digitVal :: Parser Integer
+digitVal = digit >-> digitToInt >-> fromIntegral
+
+number' :: Integer -> Parser Integer
+number' n = digitVal #> (\ d -> number' (10*n+d))
+          ! return n
+number :: Parser Integer
+number = token (digitVal #> number')
